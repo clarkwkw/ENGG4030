61901f931573f26f1c7cc48aef7aabccd28a6580
clarkwkw <clarkwkw@yahoo.com.hk>
2018-03-07 12:51:43 +0800

    update: reduce memory usage

--- hw2/q1b-job2-mapper.py
+++ hw2/q1b-job2-mapper.py
@@ -7,6 +7,7 @@ import utils
 CANDIDATE_FILE = "candidates.txt"
 
 candidate_pairs = {}
+freq_items = {}
 n_baskets = 0
 
 args = utils.parse_args()
@@ -14,15 +15,21 @@ THRESHOLD, N_ITEM = args.threshold, args.n_item
 
 with open(CANDIDATE_FILE, "r") as f:
 	for line in f:
-		candidate_pairs[line.strip().replace("\t", "-")] = 0
+		for item in line.strip().split("\t"):
+			freq_items[item] = True
 
 for line in utils.get_line():
 	unique_items = sorted(list({word: True for word in line.split()}.keys()))
 	n_baskets += 1
 	for t in utils.enumerate_recursive(unique_items, N_ITEM):
-		t = "-".join(t)
-		if t in candidate_pairs:
-			candidate_pairs[t] += 1
+		is_freq = True
+		for item in t:
+			if t not in freq_items:
+				is_freq = False
+				break
+		if is_freq:
+			t = "-".join(t)
+			candidate_pairs[t] = candidate_pairs.get(t, 0) + 1
 
 for pair, count in candidate_pairs.items():
 	print("%s\t%d\t%d"%(pair, count, n_baskets))
