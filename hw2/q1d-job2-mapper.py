e28db8acdfe63e07b1150a344592e792bc8f8dbc
clarkwkw <clarkwkw@yahoo.com.hk>
2018-03-08 17:25:07 +0800

    Revert "update: q1d"
    This reverts commit 12b37340a6c6e3f9e8f75d23957045595d043c4d.

--- hw2/q1d-job2-mapper.py
+++ hw2/q1d-job2-mapper.py
@@ -7,7 +7,6 @@ import utils
 CANDIDATE_FILE = "candidates.txt"
 
 candidate_pairs = {}
-candidate_items = {}
 n_baskets = 0
 
 args = utils.parse_args()
@@ -15,13 +14,10 @@ THRESHOLD, N_ITEM = args.threshold, args.n_item
 
 with open(CANDIDATE_FILE, "r") as f:
 	for line in f:
-		items = line.strip().split("\t")
-		for item in items:
-			candidate_items[item] = 0
-		candidate_pairs["-".join(items)] = 0
+		candidate_pairs[line.strip().replace("\t", "-")] = 0
 
 for line in utils.get_line():
-	unique_items = sorted(list({word: True for word in line.split() if word in candidate_items}.keys()))
+	unique_items = sorted(list({word: True for word in line.split()}.keys()))
 	n_baskets += 1
 	for t in utils.enumerate_recursive(unique_items, N_ITEM):
 		t = "-".join(t)
