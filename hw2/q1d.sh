2c5a79cd14ce014b9f1ed2519dd17dcad80fb4c2
clarkwkw <clarkwkw@yahoo.com.hk>
2018-03-08 13:10:32 +0800

    update: test scripts

--- hw2/q1d.sh
+++ hw2/q1d.sh
@@ -1,5 +1,7 @@
 THRESHOLD=$1
 N_ITEM=$2
+N_MAPPER=5
+N_REDUCER=5
 
 if [ "$#" -ne 2 ]; then
 	echo "usage: sh q1d.sh [threshold] [n_item]"
@@ -18,7 +20,9 @@ rm -rf ~/ENGG4030/hw2/candidates.txt ~/ENGG4030/hw2/mr_output.txt
 -reducer "q1b-job1-reducer.py $THRESHOLD $N_ITEM" \
 -input /shakespeare-basket1.txt \
 -input /shakespeare-basket2.txt \
--output /hw2_q1b_job1
+-output /hw2_q1b_job1 \
+-D mapred.map.tasks=$N_MAPPER \
+-D mapred.reduce.tasks=$N_REDUCER
 
 ./bin/hdfs dfs -copyToLocal /hw2_q1b_job1/part-00000 ~/ENGG4030/hw2/candidates.txt
 
@@ -31,7 +35,9 @@ rm -rf ~/ENGG4030/hw2/candidates.txt ~/ENGG4030/hw2/mr_output.txt
 -reducer "q1b-job2-reducer.py $THRESHOLD $N_ITEM" \
 -input /shakespeare-basket1.txt \
 -input /shakespeare-basket2.txt \
--output /hw2_q1b_job2
+-output /hw2_q1b_job2 \
+-D mapred.map.tasks=$N_MAPPER \
+-D mapred.reduce.tasks=$N_REDUCER
 
 ./bin/hdfs dfs -copyToLocal /hw2_q1b_job2/part-00000 ~/ENGG4030/hw2/mr_output.txt
 
