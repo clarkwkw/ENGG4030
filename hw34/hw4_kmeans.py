da9f7659ceec66508d029ee4b5cd588ea7113ee4
clarkwkw <clarkwkw@yahoo.com.hk>
2018-04-10 09:28:54 +0800

    fix: hw4 q1 bugs

--- hw34/hw4_kmeans.py
+++ hw34/hw4_kmeans.py
@@ -1,6 +1,8 @@
 import mr_utils
 import mnist
 import hw3_kmeans
+import argparse
+import numpy as np
 from sklearn.decomposition import PCA
 
 if __name__ == "__main__":
@@ -9,13 +11,16 @@ if __name__ == "__main__":
 	parser.add_argument("--kernel", default = "mr", choices = ["local", "mr"])
 	parser.add_argument("--n_iter", default = 15, type = int)
 	parser.add_argument("--vote_proportion", default = 0.05, type = float)
-
+	args = parser.parse_args()
 	if args.case == "train":
 		lbls, imgs = mnist.read("training")
 		pca = PCA(n_components = mr_utils.PCA_N_COMPONENTS, copy = True)
-		img = pca.fit_transform(imgs)
+		imgs = pca.fit_transform(imgs)
 		np.save("pca.npy", pca.components_.T)
-		mnist.save_img(pca.components_, "eigendigits.png")
+		try:
+			mnist.save_img(pca.components_, "eigendigits.png")
+		except:
+			print("Cannot export eigen digits image")
 		hw3_kmeans.q1a(args.kernel, n_iter = args.n_iter, train_lbl = lbls, train_img = imgs)
 	else:
 		lbls, imgs = mnist.read("testing")
